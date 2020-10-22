00ac0067639fccdb1c99a6400fcda4ab331e3139
emrahburak <gemrahburak@gmail.com>
2020-10-22 13:24:46 +0300

    mochapot-rest:0.0.4 --test post

--- post/api/urls.py
+++ post/api/urls.py
@@ -1,19 +1,37 @@
 
-from django.urls import path, include
-from post.api.views import (PostListApiView,
+from django.urls import path
+from django.views.decorators.cache import cache_page
+
+from post.api.views import (
+                            PostListApiView,
                             PostDetailApiView,
-                            PostDeleteApiView,
                             PostUpdateApiView,
-                            PostCreateApiView
-                            )
-
-app_name= 'post'
-
+                            PostCreateApiView,
+                        )
+app_name = "post"
 urlpatterns = [
-
-    path('list', PostListApiView.as_view(), name='list'),
+    path('list', cache_page(60 * 1)(PostListApiView.as_view()), name='list'),
     path('detail/<slug>', PostDetailApiView.as_view(), name='detail'),
-    path('delete/<slug>', PostDeleteApiView.as_view(), name='delete'),
     path('update/<slug>', PostUpdateApiView.as_view(), name='update'),
-    path('create/', PostCreateApiView.as_view(), name='create')
+    path('create/', PostCreateApiView.as_view(), name='create'),
 ]
+
+
+#from django.urls import path, include
+#from post.api.views import (PostListApiView,
+#                            PostDetailApiView,
+#                            PostDeleteApiView,
+#                            PostUpdateApiView,
+#                            PostCreateApiView
+#                            )
+#
+#app_name= 'post'
+#
+#urlpatterns = [
+#
+#    path('list', PostListApiView.as_view(), name='list'),
+#    path('detail/<slug>', PostDetailApiView.as_view(), name='detail'),
+#    path('delete/<slug>', PostDeleteApiView.as_view(), name='delete'),
+#    path('update/<slug>', PostUpdateApiView.as_view(), name='update'),
+#    path('create/', PostCreateApiView.as_view(), name='create')
+#]
--- post/tests.py
+++ post/tests.py
@@ -1,26 +1,114 @@
-from django.test import TestCase
+
+import json
+from rest_framework.test import APITestCase
+from django.urls import reverse
 from django.contrib.auth.models import User
-from django.utils import timezone
-from .models import Post
+from post.models import Post
 
 
-class BasicTest(TestCase):
+class UserLogin(APITestCase):
+    
+    url_login = reverse("token_obtain_pair")
+    url_create = reverse("post:create")
+    url_list = reverse("post:list")
 
     def setUp(self):
-        self.post = Post()
-        self.post.user = User(username='testAlpha',password='011305006a')
-        self.post.title = 'I am a title'
-        self.post.created = timezone.now()
-        self.post.save()
+        self.username = "testuser"
+        self.password = "testpassword123"
+        self.user = User.objects.create_user(username=self.username,
+                                             password=self.password)
 
-    def test_fields(self):
-        post = Post()
-        post.user = 'Alex'
-        post.title = 'make more test'
-        post.created = timezone.now()
-        post.save()
+        self.test_jwt_authentication()
+
+    def test_jwt_authentication(self):
+        "jwt_authentication"
+        response = self.client.post(self.url_login,
+                                    {"username" : self.username,
+                                     "password" : self.password})
+        self.assertEqual(200, response.status_code)
+        self.assertTrue("access" in json.loads(response.content))
+        self.token = response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer '+self.token)
 
 
-        record = Post.objects.get(pk=post.id)
-        self.assertEqual(record, post)
+    def test_add_new_post(self):
+        "add new post"
+        data = {
+            'title':'başlik',
+            'content':'icerik'
+            }
 
+        response = self.client.post(self.url_create, data)
+        self.assertEqual(201, response.status_code)
+
+
+    def test_add_new_post_unauthorization(self):
+        "add new post without authentication"
+        self.client.credentials()
+
+        data = {
+            'title':'baslik',
+            'content': 'icerik',
+            }
+
+        response = self.client.post(self.url_create, data)
+        self.assertEqual(401, response.status_code)
+        
+
+
+    def test_post_list(self):
+        self.test_add_new_post()
+        response = self.client.get(self.url_list)
+        self.assertTrue(len(json.loads(response.content)['results']) == Post.objects.all().count())
+
+            
+
+class PostUpdateDelete(APITestCase):
+
+    url_login = reverse("token_obtain_pair")
+    url_create = reverse("post:create")
+    url_list = reverse("post:list")
+
+    def setUp(self):
+        self.username = "testuser"
+        self.password = "testpassword123"
+        self.user = User.objects.create_user(username=self.username,
+                                             password=self.password)
+        self.user2 = User.objects.create_user(username='testuser2',
+                                              password='testpassword1234')
+
+        self.post = Post.objects.create(user=self.user,title='başlık', content='içerik')
+        self.url = reverse('post:update',kwargs={'slug':self.post.slug})
+
+        self.test_jwt_authentication()
+
+    def test_jwt_authentication(self, username='testuser', password='testpassword123'):
+        "jwt_authentication"
+        response = self.client.post(self.url_login,
+                                    {"username" : username,
+                                     "password" : password})
+        self.assertEqual(200, response.status_code)
+        self.assertTrue("access" in json.loads(response.content))
+        self.token = response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer '+self.token)
+
+
+    def test_post_delete(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(204, response.status_code)
+
+    def test_post_delete_different_user(self):
+        self.test_jwt_authentication('testuser2','testpassword1234')
+        response = self.client.delete(self.url)
+        self.assertEqual(403, response.status_code)
+
+    def test_post_update(self):
+        data = {
+
+            'title':'baslik',
+            'content':'icerik'
+            }
+
+        response = self.client.put(self.url, data)
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(Post.objects.get(id=self.post.id).content == data['content'])
